2ee8c24d61d8cafc9599b7ad3dff444bd3973768
Jeremy Leibs <jeremy@rerun.io>
2024-02-03 18:57:13 +0100

    Increase camera height

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -220,7 +220,7 @@ public class Constants {
         public static final PoseStrategy kFallbackVisionStrategy = PoseStrategy.LOWEST_AMBIGUITY;
         // Cam mounted facing forward, half a meter forward of center, half a meter up from center.
         public static final Transform3d kRobotToCam =
-                new Transform3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d(0, 0, 0));
+                new Transform3d(new Translation3d(0.0, 0.0, 1.0), new Rotation3d(0, 0, 0));
 
         // // The layout of the AprilTags on the field
         public static final AprilTagFieldLayout kFieldTagLayout =

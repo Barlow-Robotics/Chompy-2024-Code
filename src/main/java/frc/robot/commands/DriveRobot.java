1a54c0d609915e065f5f3f868b400065916d0bf8
Em Ploss <ehploss@gmail.com>
2024-02-13 19:05:26 -0500

    removed controller check in driverobot

--- src/main/java/frc/robot/commands/DriveRobot.java
+++ src/main/java/frc/robot/commands/DriveRobot.java
@@ -60,15 +60,15 @@ public class DriveRobot extends Command {
         // and the chassis (positive X is forward, Positive Y is left), we use the controller X input as the drive Y input
         // and the controller Y input as the drive X input.
         
-        if (DriverStation.getJoystickName(ElectronicsIDs.DriverControllerPort).equals("Logitech Extreme 3D")) {
+        // if (DriverStation.getJoystickName(ElectronicsIDs.DriverControllerPort).equals("Logitech Extreme 3D")) {
             rawX = -driverController.getRawAxis(this.ControllerYSpeedID);  
             rawY = -driverController.getRawAxis(this.ControllerXSpeedID);
             rawRot = -driverController.getRawAxis(this.ControllerRotID); 
-        } else {
-            rawX = driverController.getRawAxis(this.ControllerYSpeedID);  
-            rawY = -driverController.getRawAxis(this.ControllerXSpeedID);
-            rawRot = -driverController.getRawAxis(this.ControllerRotID); 
-        }
+        // } else {
+        //     rawX = driverController.getRawAxis(this.ControllerYSpeedID);  
+        //     rawY = -driverController.getRawAxis(this.ControllerXSpeedID);
+        //     rawRot = -driverController.getRawAxis(this.ControllerRotID); 
+        // }
 
         SpeedX = MathUtil.applyDeadband(rawX, DeadBand) * DriveConstants.MaxDriveableVelocity;
         SpeedY = MathUtil.applyDeadband(rawY, DeadBand) * DriveConstants.MaxDriveableVelocity;

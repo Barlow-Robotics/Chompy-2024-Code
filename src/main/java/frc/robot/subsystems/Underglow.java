1c396c1dc8dc829d2370b3f8940b44e9e661b091
BArlow RobAutics Dell 1 <joelbarlowrobotics@gmail.com>
2024-02-25 20:27:00 -0500

    Update Underglow.java
    Added if statements for each subsystem to match with LEDs

--- src/main/java/frc/robot/subsystems/Underglow.java
+++ src/main/java/frc/robot/subsystems/Underglow.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.subsystems.ShooterMount.ShooterMountState;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,9 +18,16 @@ public class Underglow extends SubsystemBase {
     public boolean LEDHumanSource = false;
     public boolean LEDHumanFloor = false;
 
+    Shooter shooterSub;
+    ShooterMount shooterMountSub;
+
     SerialPort port = null;
     
     int currentMode = 1;
+
+    public Underglow(Shooter s) {
+        shooterSub = s;
+    }
 
 
     public Underglow() {
@@ -52,9 +60,14 @@ public class Underglow extends SubsystemBase {
             // desiredMode += Constants.UnderGlowConstants.RedAlliance;  
         if (LEDHumanSource == true) {
             desiredMode += Constants.UnderGlowConstants.RobotSource;
-        } else if (LEDHumanFloor == true)
-        {
+        } else if (LEDHumanFloor == true){
             desiredMode += Constants.UnderGlowConstants.RobotFloorSource;
+        } else if (shooterSub.isNoteLoaded() == true) {
+            desiredMode += Constants.UnderGlowConstants.NoteLoaded;
+        } else if (shooterSub.isShooting() == true) {
+            desiredMode += Constants.UnderGlowConstants.Shooting;
+        } else if (shooterMountSub.getShooterMountState() == ShooterMountState.Climb) {
+            desiredMode += Constants.UnderGlowConstants.Climbing;
         }
         boolean check = (byte) (desiredMode & byte2) == byte2;
         // System.out.println("************************************************Current Check for arduino: " + check +  desiredMode);
